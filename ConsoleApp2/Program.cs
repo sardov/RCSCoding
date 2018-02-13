76c97598536ce207139dbbc936d52220cf741c91
sardov <sardov.k@gmail.com>
2018-02-13 21:36:16 +0200

    hello word app

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // izsaucam sevis izveidoto funkciju
+            SayHello();
+            // izsaucam savu otro izveidoto funkciju
+            SayHelloToUser();
+        }
+
+        static void SayHelloToUser()
+        {
+            // palūdzam lietotājam ievadīt savu vārdu
+            // uzrakstot lūgumu ekrāna
+            Console.WriteLine("Please enter your name:");
+            // izveidojam mainīgo, kur glabāt lietotāja ievadīto vērtību
+            string usersName;
+            // paprasam konsolei ievadīt tekstu, saglabajām rezultātu mainīgajā
+            usersName = Console.ReadLine();
+            Console.WriteLine("Labdien," + usersName + "!");
+                
+            Console.ReadLine();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+    }
+}
