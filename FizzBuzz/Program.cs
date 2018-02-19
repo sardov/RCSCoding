bcd0cc1abab708cc301a19b1c08a85b40e85eb0d
sardov <sardov.k@gmail.com>
2018-02-19 17:34:35 +0200

    fizzbuzz try

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (int i % 3 = true)
+            {
+                Console.Writeline("Fizz");
+            }
+                else if (i / 5)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (//dalas ar 3 un ar 5)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (//i ne dalas ne ar 3 ne ar 5)
+                    
+            {
+                Console.WriteLine(int i);
+            }
+        }
+
+
+
+
+
+
+    }
+
+}
+
+
+}
+
+
+
